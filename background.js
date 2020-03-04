74bd98cfca0f21a63a4a57d231eed24aa557db8a
rjha18 <rjha18@live.com>
2020-03-03 17:39:23 -0800

    Working route

--- background.js
+++ background.js
@@ -1,14 +1,38 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ color: '#3aa757' }, function () {
-        console.log("The color is green.");
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'developer.chrome.com' },
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) { 
+    url = JSON.stringify(details.url)
+    if (url.includes("123456") || url.includes("css") || url.includes("style") || url.includes("jpg") || url.includes("jpeg") || url.includes("png") || url.includes("js")) {
+      return {cancel: false};
+    } else {
+      return {redirectUrl: "http://localhost:3000?url=" + details.url}
+    }
+  },
+  {urls: ["https://*/*"]},
+  ["blocking"]);
+
+
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//     //alert('updated from background');
+//     chrome.tabs.sendMessage( tabId, {
+//         message: 'hello!',
+//         url: changeInfo.url
+//       })
+// });
+
+// chrome.runtime.onInstalled.addListener(function () {
+//     chrome.storage.sync.set({ color: '#3aa757' }, function () {
+//         console.log("The color is green.");
+//     });
+//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+//         chrome.declarativeContent.onPageChanged.addRules([{
+//             conditions: [new chrome.declarativeContent.PageStateMatcher({
+//                 pageUrl: { hostEquals: 'developer.chrome.com' },
+//             })
+//             ],
+//             actions: [new chrome.declarativeContent.ShowPageAction()]
+//         }]);
+//     });
+//     chrome.tabs.opUpdated.addListener(function (tabId, changeInfo, tab) {
+
+//     })
+// });
---
+++ stall.js
@@ -0,0 +1,11 @@
+//setTimeout(() => {window.stop()}, 10000);
+
+
+// chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
+//       // listen for messages sent from background.js
+//       if (request.message === 'hello!') {
+//         //alert("Wooooo!");
+//         setTimeout("", 5000);
+//       }
+//   });
